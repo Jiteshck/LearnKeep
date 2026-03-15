21e4b174e6083694024982bc2bb14003cf8fb0fc
Jitesh Choudhary <Jiteshck193@gmail.com>
2026-03-15 13:55:59 +0530

    Deployed server on render

--- app/src/main/java/com/example/learnkeep/api/ApiClient.java
+++ app/src/main/java/com/example/learnkeep/api/ApiClient.java
@@ -23,7 +23,7 @@ public class ApiClient {
                     .build();
 
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://192.168.1.3:3000/")
+                    .baseUrl("https://learnkeep.onrender.com/")
                     .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
