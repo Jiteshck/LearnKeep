d6f2566b30a0c92ca61291a0d586398637acb67b
Jitesh Choudhary <Jiteshck193@gmail.com>
2026-04-03 22:26:54 +0530

    Fix mail service for Render(IPv4)

--- learnkeep-server/emailService.js
+++ learnkeep-server/emailService.js
@@ -1,20 +1,27 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-service: "gmail",
-auth: {
-user: process.env.EMAIL_USER,
-pass: process.env.EMAIL_PASS
-}
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    family: 4,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
 });
-
 async function sendEmail(to, subject, text){
-await transporter.sendMail({
-from: `"LearnKeep" <${process.env.EMAIL_USER}>`,
-to,
-subject,
-text
-});
+    try {
+        await transporter.sendMail({
+            from: `"LearnKeep" <${process.env.EMAIL_USER}>`,
+            to,
+            subject,
+            text
+        });
+        console.log("✅ Email sent to:", to);
+    } catch (error) {
+        console.error("❌ Email error:", error);
+    }
 }
-
 module.exports = sendEmail;
